8b92b90d4e97cebc977ef48f16a0737500ed3929
aristotelos <arisvd@gmail.com>
2024-10-22 15:17:52 +0200

    chore: Add basic integration tests
    Assert that the extension can be activated.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,23 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+// import * as extension from "../../extension";
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+	test("Extension can be found by full ID and has expected properties", () => {
+        const extension = vscode.extensions.getExtension("aristotelos.cucumber-test-extension");
+
+        assert.ok(extension);
+        assert.equal(extension.extensionKind, vscode.ExtensionKind.UI);
+    });
+
+    test("Extension can be activated", async () => {
+        const extension = vscode.extensions.getExtension("aristotelos.cucumber-test-extension");
+
+        await extension?.activate();
+
+        assert.ok(extension?.isActive);
+    });
 });
